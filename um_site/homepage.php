7d88374bc0d8e56b62bcf228c4ded650c56395af
ajpeppers <ajpeppersjr@gmail.com>
2024-04-05 14:43:41 -0400

    Added cards in homepage; implemented login redirection

--- um_site/homepage.php
+++ um_site/homepage.php
@@ -1,3 +1,11 @@
+<!--This stores the e-mail used to log-in (so we can query with the given e-mail) -->
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+}
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,7 +17,7 @@
   <meta name="keywords" content="CS 3250, Upsorn, Praphamontripong, Software Testing">
   <link rel="icon" href="https://www.cs.virginia.edu/~up3f/cs3250/images/st-icon.png" type="image/png" />  
   
-  <title>Maintenance Services</title>
+  <title>UniMeet</title>
   <link href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">  
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  
@@ -57,7 +65,7 @@
               Account
           </div>
         </a>
-        <a class="nav-item nav-link active" href="#">
+        <a class="nav-item nav-link active" href="login.php">
           <div class="icon-wrapper">
               <i class="mdi mdi-logout icon"></i>
           </div>
@@ -68,107 +76,39 @@
       </div>
     </div>
   </nav>
-<div class="container">
-  
-  <!---------------->
 
-  <form method="post" action="" onsubmit="return validateInput()">
-    <table style="width:98%">
-      <tr>
-        <td width="50%">
-          <div class='mb-3'>
-            Requested date:
-            <input type='text' class='form-control' 
-                   id='requestedDate' name='requestedDate' 
-                   placeholder='Format: yyyy-mm-dd' 
-                   pattern="\d{4}-\d{1,2}-\d{1,2}" 
-                   value="" />
-          </div>
-        </td>
-        <td>
-          <div class='mb-3'>
-            Room Number:
-            <input type='text' class='form-control' id='roomNo' name='roomNo' 
-                   value="" />
-          </div>
-        </td>
-      </tr>
-      <tr>
-        <td colspan=2>
-          <div class='mb-3'>
-            Requested by: 
-            <input type='text' class='form-control' id='requestedBy' name='requestedBy'
-                   placeholder='Enter your name'
-                   value="" />
-          </div>
-        </td>
-      </tr>
-      <tr>
-        <td colspan=2>
-          <div class="mb-3">
-            Description of work/repair:
-            <input type='text' class='form-control' id='requestDesc' name='requestDesc'
-                   value="" />
+<div class="row justify-content-center mt-4">
+  <div class="col">
+    <h3 class="row justify-content-center">My Events</h3>
+    <div class="card">
+      <div class="row">
+        <div class="col">
+          <h4 class="card-title event-name">Event Name</h4>
+          <h6 class="card-subtitle mb-2">Location</h6>
+          <h6 class="card-subtitle mb-2">Club</h6>
         </div>
-        </td>
-      </tr>
-      <tr>
-        <td colspan=2>
-          <div class='mb-3'>
-            Requested Priority:
-            <select class='form-select' id='priority_option' name='priority_option'>
-              <option selected></option>
-              <option value='high' >
-                High - Must be done within 24 hours</option>
-              <option value='medium' >
-                Medium - Within a week</option>
-              <option value='low' >
-                Low - When you get a chance</option>
-            </select>
-          </div>
-        </td>
-      </tr>
-    </table>
-
-    <div class="row g-3 mx-auto">    
-      <div class="col-4 d-grid ">
-      <input type="submit" value="Add" id="addBtn" name="addBtn" class="btn btn-dark"
-           title="Submit a maintenance request" />                  
-      </div>	    
-      <div class="col-4 d-grid ">
-      <input type="submit" value="Confirm update" id="cofmBtn" name="cofmBtn" class="btn btn-primary"
-           title="Update a maintenance request" />                  
-      </div>	    
-      <div class="col-4 d-grid">
-        <input type="reset" value="Clear form" name="clearBtn" id="clearBtn" class="btn btn-secondary" />
-      </div>      
-    </div>  
-    <div>
-  </div>  
-  </form>
-
+        <div class="col text-end card-right-col">
+          <h3 class="card-time card-title text-right">7:00</h3>
+          <h6 class="card-subtitle mb-2 text-right">4/5/24</h6>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <h3 class="row justify-content-center">My Clubs</h3>
+    <div class="card">
+      <div class="row">
+        <div class="col">
+          <h4 class="card-title event-name">Club Name</h4>
+          <h6 class="card-subtitle mb-2">Club Category</h6>
+        </div>
+        <div class="col text-end card-right-col">
+          <h3 class="card-time card-title text-right">PRIV</h3>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
-
-
-<hr/>
-<div class="container">
-<h3 class="row justify-content-center">List of Events</h3>
-<div class="row justify-content-center">  
-<table class="w3-table w3-bordered w3-card-4 center" style="width:100%">
-  <thead>
-  <tr style="background-color:#B0B0B0">
-    <th width="30%"><b>ReqID</b></th>
-    <th width="30%"><b>Date</b></th>        
-    <th width="30%"><b>Room#</b></th> 
-    <th width="30%"><b>By</b></th>
-    <th width="30%"><b>Description</b></th>        
-    <th width="30%"><b>Priority</b></th> 
-    <th><b>Update?</b></th>
-    <th><b>Delete?</b></th>
-  </tr>
-  </thead>
-</table>
-</div>   
 
 
 <br/><br/>
