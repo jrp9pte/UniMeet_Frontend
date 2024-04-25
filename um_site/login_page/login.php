39d7935819b92417d5c843e95019bedf1044cdbb
Jay Patel <jrp9pte@virginia.edu>
2024-04-24 23:24:19 -0400

    small change

--- um_site/login_page/login.php
+++ um_site/login_page/login.php
@@ -57,7 +57,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['signup-button'])) {
   <link href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">  
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  
-  <link rel="stylesheet" href="unimeet.css">  
+  <link rel="stylesheet" href="../unimeet.css">  
 </head>
 
 <body>  
